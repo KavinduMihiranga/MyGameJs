4ae604fd2556ddd34fb6baf6655b3f53bcd9f2ba
KavinduMihiranga <mihirangak1999@gmail.com>
2022-05-15 15:53:07 +0530

    added function and update game

--- script.js
+++ script.js
@@ -1,97 +1,169 @@
 $(document).ready(function () {
-    let balloons=$(".balloon");
+    let balloons = $(".balloon");
+
     let balloon1 = $("#balloon1");
     let balloon2 = $("#balloon2");
     let balloon3 = $("#balloon3");
     let balloon4 = $("#balloon4");
     let balloon5 = $("#balloon5");
+
+    let balloon6 = $("#balloon6");
+    let balloon7 = $("#balloon7");
+    let balloon8 = $("#balloon8");
+    let balloon9 = $("#balloon9");
+    let balloon10 = $("#balloon10");
+
+    $("#balloon6").hide();
+    $("#balloon7").hide();
+    $("#balloon8").hide();
+    $("#balloon9").hide();
+    $("#balloon10").hide();
+
     let gameDiv = $("#gameDiv");
-    let btnStart= $("#startDiv");
-    let scoreDiv=$("#scoreDiv");
-    let lineDiv=$("#lineDiv");
-
-    let score=0;
-    
-    let containerWidth = parseInt(gameDiv.width());
-    let containerHeight = parseInt(gameDiv.height());
-   
-
+    let btnStart = $("#startDiv");
+    let scoreDiv = $("#scoreDiv");
+    let lineDiv = $("#lineDiv");
+    let score = 1;
+    let speed = 5;
+    let gameOver = false;
     deleteBalloon();
     flowDownBalloon();
     restart();
+    repeat();
+
+    let balloonHeight = parseInt(gameDiv.height());
+    let balloonWidth = parseInt(gameDiv.width());
 
     function restart() {
-        if (gameDiv==lineDiv){
+        if (gameDiv == lineDiv) {
             gameDiv.text("Game Over");
         }
     }
 
+
     function deleteBalloon() {
-        $("#balloon1").click( function () {
+        $("#balloon1").click(function () {
             $(this).hide();
             score++;
-            scoreDiv=score;
+            scoreDiv = score;
+            $("#balloon10").show();
         });
 
-        $("#balloon2").click( function () {
+        $("#balloon2").click(function () {
             $(this).hide();
             score++;
-            scoreDiv=score;
+            scoreDiv = score;
+            $("#balloon9").show();
         });
 
-        $("#balloon3").click( function () {
+        $("#balloon3").click(function () {
             $(this).hide();
-            scoreDiv=0;
-            scoreDiv.text(parseInt(scoreDiv.text)+1);
+            scoreDiv = 0;
+            $("#balloon8").show();
         });
 
-        $("#balloon4").click( function () {
-            $(this).hide();
-            score++;
-            scoreDiv=score;
-        });
-
-        $("#balloon4").click( function () {
+        $("#balloon4").click(function () {
             $(this).hide();
             score++;
-            scoreDiv=score;
+            scoreDiv = score;
+            $("#balloon7").show();
         });
 
-        $("#balloon5").click( function () {
+        $("#balloon5").click(function () {
             $(this).hide();
             score++;
-            scoreDiv=score;
+            scoreDiv = score;
+            $("#balloon6").show();
         });
+
+        $("#balloon6").click(function () {
+            $(this).hide();
+            score++;
+            scoreDiv = score;
+            $("#balloon1").show();
+        });
+        $("#balloon7").click(function () {
+            $(this).hide();
+            score++;
+            scoreDiv = score;
+            $("#balloon2").show();
+        });
+        $("#balloon8").click(function () {
+            $(this).hide();
+            score++;
+            scoreDiv = score;
+            $("#balloon3").show();
+        });
+        $("#balloon9").click(function () {
+            $(this).hide();
+            score++;
+            scoreDiv = score;
+            $("#balloon2").show();
+        });
+        $("#balloon10").click(function () {
+            $(this).hide();
+            score++;
+            scoreDiv = score;
+            $("#balloon1").show();
+        });
+
 
     }
 
     function flowDownBalloon() {
-       $("#startDiv").click(function () {
-           let speed1=100;
-            $("#balloon1").animate({bottom:"-0vh"},speed1 *speed1,"linear");
+        $("#startDiv").click(function () {
+            let speed1 = 100;
+            $("#balloon1").animate({bottom: "-0vh"}, speed1 * speed1, "linear");
             speed1++;
-            
-       })
+
+        })
         $("#startDiv").click(function () {
-            let speed2=125;
-            $("#balloon2").animate({bottom:"-0vh"},speed2*speed2,"linear");
+            let speed2 = 125;
+            $("#balloon2").animate({bottom: "-0vh"}, speed2 * speed2, "linear");
             speed2++;
-       })
+        })
         $("#startDiv").click(function () {
-            let speed3=150;
-            $("#balloon3").animate({bottom:"-0vh"},speed3*speed3,"linear");
+            let speed3 = 150;
+            $("#balloon3").animate({bottom: "-0vh"}, speed3 * speed3, "linear");
             speed3++;
-       })
+        })
         $("#startDiv").click(function () {
-            let speed4=175;
-            $("#balloon4").animate({bottom:"-0vh"},speed4*speed4,"linear");
+            let speed4 = 175;
+            $("#balloon4").animate({bottom: "-0vh"}, speed4 * speed4, "linear");
             speed4++;
-       })
+        })
         $("#startDiv").click(function () {
-            let speed5=200;
-            $("#balloon5").animate({bottom:"-0vh"},speed5*speed5,"linear");
+            let speed5 = 200;
+            $("#balloon5").animate({bottom: "-0vh"}, speed5 * speed5, "linear");
             speed5++;
-       })
+        })
+        $("#startDiv").click(function () {
+            let speed6 = 250;
+            $("#balloon6").animate({bottom: "-0vh"}, speed6 * speed6, "linear");
+            speed6++;
+        })
+        $("#startDiv").click(function () {
+            let speed7 = 150;
+            $("#balloon7").animate({bottom: "-0vh"}, speed7 * speed7, "linear");
+            speed7++;
+        })
+        $("#startDiv").click(function () {
+            let speed8 = 100;
+            $("#balloon8").animate({bottom: "-0vh"}, speed8 * speed8, "linear");
+            speed8++;
+        })
+        $("#startDiv").click(function () {
+            let speed9 = 120;
+            $("#balloon9").animate({bottom: "-0vh"}, speed9 * speed9, "linear");
+            speed9++;
+        })
+        $("#startDiv").click(function () {
+            let speed10 = 120;
+            $("#balloon10").animate({bottom: "-0vh"}, speed10 * speed10, "linear");
+            speed10++;
+        })
     }
+
+
 });
 
